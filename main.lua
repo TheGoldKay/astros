67c1fc7da3edb08c6e281307bece10a26e68041d
TheGoldKay <jhonnatanwfc@live.com>
2022-11-15 19:40:43 -0300

    trianglge angled

--- main.lua
+++ main.lua
@@ -7,3 +7,16 @@ function love.keypressed(key)
         love.event.quit()
     end 
 end 
+
+function triangle (mode, x, y, length, width , angle) -- position, length, width and angle
+	love.graphics.push()
+	love.graphics.translate(x, y)
+	love.graphics.rotate( angle )
+	love.graphics.polygon(mode, -length/2, -width /2, -length/2, width /2, length/2, 0)
+	love.graphics.pop() 
+end
+
+function love.draw()
+    x, y, angle = 200, 100, math.pi/4
+    triangle ("fill", x, y, 20, 10 , angle)
+end 
